2c69277a767fe65a50d07596909a9895ba149233
excabbage <zlijv@connect.ust.hk>
2025-05-12 17:14:10 +0800

    Update and rename Experiment.py to Main_part.py

--- Main_part.py
+++ Main_part.py
@@ -5,12 +5,13 @@ from mpl_toolkits.mplot3d import Axes3D
 import tic_tac_toe
 from tic_tac_toe import State, State_test, AI_agent, TD
 
+
 Task = TD() 
 S = State() #initial State
 h = S.hash()[0] #get the initial State hash value
 reward1 = [] #storage the v(S) for player1
 reward2 = [] #storage the v(S) for player2
-for i in range(571960,1000000):
+for i in range(1000000,1200000):
     Task.Q_learning(i)
     pi = Task.player1.policy[h] #pi(S)
     reward1.append(Task.player1.Q_value.get(h + str(pi[0]) + str(pi[1]),0))
@@ -36,3 +37,5 @@ plt.figure()
 ax = plt.axes(projection='3d')
 ax.plot_surface(X, Y, Z, alpha=0.5, cmap='winter')
 plt.show()
+
+
